73a21c4552c0d5db33c14751b74be8f98f5bcb47
JayKabutarwala <59605424+jaykabu@users.noreply.github.com>
2020-10-16 17:29:37 +0530

    simple notepad

---
+++ src/AddNote.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+const AddNote = (props) => {
+    return (
+        <>
+            <div className={"container-fluid nav_bg"}>
+                <div className={'row'}>
+                    <div className={'col-10 mx-auto'}>
+                        <div className={'todo_style'}>
+                            <i className="fa fa-times" aria-hidden="true" onClick={()=>{
+                                props.passNote(props.id)
+                            }}/>
+                            {props.text}
+                            {/*<button onClick={''}><DeleteIcon/></button>*/}
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default AddNote;
--- src/Note.jsx
+++ src/Note.jsx
@@ -1,66 +1,79 @@
 import React, {useState} from "react";
-import Button from '@material-ui/core/Button';
-import AddIcon from '@material-ui/icons/Add';
-import NoteList from "./NoteList";
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import TextField from "@material-ui/core/TextField";
+import AddNote from "./AddNote";
+import {NavLink} from "react-router-dom";
 
 const Note = () => {
 
-    const [inputList , setInputList] = useState();
-    const [item , setItem] = useState([]);
+    const [inputList, setInputList] = useState();
 
-    const addItem = (event)=>{
-        setInputList(event.target.value)
+    const [item, setItems] = useState([]);
+
+    const inputEvent = (event) => {
+        setInputList(event.target.value);
     }
 
-    const inputSubmit = ()=>{
-        setItem((oldData)=>{
-            return [...oldData , inputList]
-        })
+    const inputSubmit = () => {
+        setItems((oldData) => {
+            return [...oldData, inputList];
+        });
         setInputList('');
     }
 
-    const deleteItem = (id)=>{
-        setItem((prevState)=>{
-           return  prevState.filter((arrElem , index)=>{
-                    return index !== id ;
+    const deleteItem = (id) => {
+        setItems((preData) => {
+            return preData.filter((arrElem, index) => {
+                return index !== id;
             })
         })
     }
-
     return (
         <>
-            <div className={'my-3'}>
-                <h1 className={'text-center'}>Note</h1>
-            </div>
-            <div className={'container contact_div'}>
-                <div className={'row'}>
-                    <div className={'col-md-3 col-6 mx-auto'}>
-                        <form onClick={''}>
-                            <div className="input-group mb-3">
-                                <label htmlFor="exampleFormControlInput1" className="form-label"></label>
-                                <input type="text" className="form-control" id="exampleFormControlInput1"
-                                       name={'inputList'} value={inputList} onChange={addItem}
-                                       placeholder={'Type a note'} autoComplete={'off'}
+            <div className={"container-fluid nav_bg Ndiv"}>
+                <div className={"row"}>
+                    <div className={"col-10 mx-auto"}>
+                        <div className={"row"}>
+                            <div
+                                className={"col-md-4 mx-auto  d-flex  flex-column"}>
+                                <h3 className={"note_div"}>NotePad</h3>
+                                <TextField
+                                    type={"text"}
+                                    placeholder={"enter a value"}
+                                    name={"content"}
+                                    onChange={inputEvent}
+                                    value={inputList}
                                 />
-                                <Button className={''} onClick={inputSubmit}>
-                                    <AddIcon className={''}/>
-                                </Button>
-                            </div>
 
-                           <div>
-                                   {/*<li>{inputList}</li>*/}
-                                   {item.map((val,index)=>{
-                                       // return  <li>{val}</li>
-                                    return   <NoteList text={val} key={index} id={index} passnote={deleteItem}/>
-                                   })}
-                           </div>
-                        </form>
+
+                                <Button onClick={inputSubmit}>
+                                    <AddIcon/>
+                                </Button>
+
+                                {/*<li>{inputList}</li>*/}
+                                {item.map((value, index) => {
+                                    // return  <li>{value}</li>
+                                    return <AddNote
+                                        text={value}
+                                        id={index}
+                                        key={index}
+                                        passNote={deleteItem}
+                                    />
+                                })}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
 
+            <div style={{textAlign:'center'}}>
+                <NavLink to={'/anotepad'}> Go To Advance Notepad </NavLink>
+            </div>
         </>
     )
+
 };
+
 
 export default Note;
